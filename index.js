5be057f18c7528193025bb697b98de340843516f
Sashenka Justin <sjustin@deloitte.com.au>
2024-08-15 14:38:32 +1000

    Update data file

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var coreFrontendData; 
-var advancedFrontendData;
-var x;
-var y; 
+var coreFrontendData, advancedFrontendData, cssData, frameworksLibrariesData, testingData, buildToolsData, dataIntegrationData, cmsData, otherData
+var x, y;
 var xAxis;
 
 // set the dimensions and margins of the graph
@@ -19,10 +17,17 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // get all survey data 
-d3.csv("https://raw.githubusercontent.com/sash-enka/fed-survey/main/testData.csv").then(data => {
+d3.csv("https://raw.githubusercontent.com/sash-enka/fed-survey/main/averages.csv").then(data => {
     // split data into topics 
-    coreFrontendData = data.slice(0, 2);
-    advancedFrontendData = data.slice(2, 5);
+    coreFrontendData = data.slice(0, 5);
+    advancedFrontendData = data.slice(5, 10);
+    cssData = data.slice(10, 15);
+    frameworksLibrariesData = data.slice(15, 20);
+    testingData = data.slice(20, 24);
+    buildToolsData = data.slice(24, 29);
+    dataIntegrationData = data.slice(29, 33);
+    cmsData = data.slice(33, 37);
+    otherData = data.slice(37, 46);
 
     // Add x axis - 
     x = d3.scaleBand()
@@ -66,7 +71,28 @@ function update(currData) {
 function showCoreFrontend() {
     update(coreFrontendData);
 }
-
 function showAdvancedFrontend() {
     update(advancedFrontendData);
 }
+function showCSS() {
+    update(cssData)
+}
+function showFrameworksLibraries() {
+    update(frameworksLibrariesData)
+}
+function showTesting() {
+    update(testingData)
+}
+function showBuildTools() {
+    update(buildToolsData)
+}
+function showDataIntegration() {
+    update(dataIntegrationData)
+}
+function showCMS() {
+    update(cmsData)
+}
+function showOther() {
+    update(otherData)
+}
+
